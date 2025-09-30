1afa65971a29eb5645a0bdc7480829576aba4731
DESKTOP-JQ9OQ1I\Rubén <rcalvom2501@educantabria.es>
2025-09-30 20:59:38 +0200

    no message

--- UD1_Actividad_01/src/Main.java
+++ UD1_Actividad_01/src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Ejercicio 1
+
         System.out.println("Buenos dias futuros programadores");
 
         /*
