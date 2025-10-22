6b83262776625108736a1b0900e4551ad4e8a1de
COMPU-S027\daw1 <rubencalvomuriedas@gmail.com>
2025-10-22 09:47:41 +0200

    22/10/2025

--- Primer Trismestre/Actividad 4 N1-14/E13.java
+++ Primer Trismestre/Actividad 4 N1-14/E13.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class E13 {
     public static void main(String[] args) {
 
@@ -7,6 +9,18 @@ public class E13 {
     e I=2, la secuencia sería 1, 3, 5, 7, 9… Con V=7 e I=10, la secuencia sería 7, 17, 27, 37… El
     programa solicitará al usuario V, I además de N (nº de valores a crear).
      */
+    Scanner sc = new Scanner(System.in);
+    System.out.println("introduce el valor Inicial V: ");
+    int vInicial = sc.nextInt();
+    System.out.println("introduce el incremento I: ");
+    int iInicial = sc.nextInt();
+    System.out.println("Introduce la cantidad de valores que se desean obtener: ");
+    int cantidadN = sc.nextInt();
+
+    int[] valores = new int[cantidadN];
+    valores [0] = vInicial;
+    for(int i = 1;
+    valores[i] =
 
     }
 }
