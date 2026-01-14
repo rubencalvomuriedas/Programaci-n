49eddd171db7bccfc7d429122cc1df1c5013b565
COMPU-S027\daw1 <rubencalvomuriedas@gmail.com>
2026-01-14 11:24:28 +0100

    Agenda acabada

--- Segundo Trismestre/Agenda/Agenda/src/Agenda.java
+++ Segundo Trismestre/Agenda/Agenda/src/Agenda.java
@@ -1,2 +1,91 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Agenda {
+    private ArrayList<Contacto> contactos;
+
+    public Agenda() {
+        contactos = new ArrayList<>();
+    }
+
+    public void añadirContacto() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nombre: ");
+        String nombre = sc.nextLine();
+
+        if (!Contacto.validarNombre(nombre)){
+            System.out.println("Nombre Incorrecto. ");
+            return;
+        }
+
+        if (existeContacto(nombre)){
+            System.out.println("El contacto ya existe. ");
+            return;
+        }
+
+        System.out.println("correo: ");
+        String correo = sc.nextLine();
+
+        if (!Contacto.validarCorreo(correo)){
+            System.out.println("Correo incorrecto. ");
+        }
+
+        String telefono = null;
+        Contacto c = new Contacto(nombre, telefono, correo);
+        contactos.add(c);
+        System.out.println("Contacto añadido correctamente. ");
+    }
+
+    public void buscarContacto(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el nombre que deseas buscar: ");
+        String nombre = sc.nextLine();
+
+        for (int i = 0; i < contactos.size(); i++){
+            if (contactos.get(i).getNombre().equalsIgnoreCase(nombre)){
+                System.out.println(contactos.get(i));
+                return;
+            }
+            System.out.println("Contacto no encontrado. ");
+        }
+    }
+
+    public void eliminarContacto() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nombre a eliminar: ");
+        String nombre = sc.nextLine();
+
+        for (int i = 0; i < contactos.size(); i++) {
+            if (contactos.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                contactos.remove(i);
+                System.out.println("Contacto eliminado.");
+                return;
+            }
+        }
+        System.out.println("Contacto no encontrado.");
+    }
+
+    public void mostrarAgenda() {
+        if (contactos.isEmpty()) {
+            System.out.println("La agenda está vacía.");
+        } else {
+            for (int i = 0; i < contactos.size(); i++) {
+                System.out.println(contactos.get(i));
+            }
+        }
+    }
+
+    public int numeroContactos() {
+        return contactos.size();
+    }
+
+    private boolean existeContacto(String nombre) {
+        for (int i = 0; i < contactos.size(); i++) {
+            if (contactos.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- Segundo Trismestre/Agenda/Agenda/src/Main.java
+++ Segundo Trismestre/Agenda/Agenda/src/Main.java
@@ -1,15 +1,48 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Agenda agenda = new Agenda();
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+
+        while (opcion != 6) {
+
+            System.out.println("\n----- Menu -----");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Buscar contacto");
+            System.out.println("3. Eliminar contacto");
+            System.out.println("4. Visualizar agenda");
+            System.out.println("5. Todos los contactos");
+            System.out.println("6. Salir");
+            System.out.println("Opcion: ");
+
+            opcion= sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion){
+                case 1:
+                    agenda.añadirContacto();
+                    break;
+                case 2:
+                    agenda.buscarContacto();
+                    break;
+                case 3:
+                    agenda.eliminarContacto();
+                    break;
+                case 4:
+                    agenda.mostrarAgenda();
+                    break;
+                case 5:
+                    System.out.println("Contactos: " + agenda.numeroContactos());
+                    break;
+                case 6:
+                    System.out.println("Fin del programa");
+                    break;
+                default:
+                    System.out.println("opcion incorrecta");
+            }
         }
     }
 }
